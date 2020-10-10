e4832af83c70daa532e1066abfc5fd440d788778
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2020-10-10 10:28:05 +0100

    removed underscore from template calls

--- tests/test_script.py
+++ tests/test_script.py
@@ -8,7 +8,7 @@ import test_constants
 with open("test_config.yaml", 'r') as yaml_file:
    params = yaml.load(yaml_file, yaml.SafeLoader)
 
-person_template = config_template._Template(
+person_template = config_template.Template(
     fields=[
         config_field.Field(name=test_constants.Constants.NUM_ARMS, types=[int], key=test_constants.Constants.NUM_ARMS, requirements=[lambda x: x > 0]),
         config_field.Field(name=test_constants.Constants.NUM_LEGS, types=[int], key=test_constants.Constants.NUM_LEGS, requirements=[lambda x: x > 0])
@@ -16,7 +16,7 @@ person_template = config_template._Template(
     level=[test_constants.Constants.PERSON]
 )
 
-cat_template = config_template._Template(
+cat_template = config_template.Template(
     dependent_variables = [test_constants.Constants.ANIMAL_TYPE],
     dependent_variables_required_values = [[test_constants.Constants.CAT]],
     fields=[
@@ -25,7 +25,7 @@ cat_template = config_template._Template(
     level=[test_constants.Constants.ANIMAL, test_constants.Constants.CAT]
 )
 
-dog_template = config_template._Template(
+dog_template = config_template.Template(
     dependent_variables = [test_constants.Constants.ANIMAL_TYPE],
     dependent_variables_required_values = [[test_constants.Constants.DOG]],
     fields=[
@@ -34,7 +34,7 @@ dog_template = config_template._Template(
     level=[test_constants.Constants.ANIMAL, test_constants.Constants.DOG]
 )
 
-animal_template = config_template._Template(
+animal_template = config_template.Template(
     fields=[
         config_field.Field(name=test_constants.Constants.TYPE, types=[str], key=test_constants.Constants.ANIMAL_TYPE, 
         requirements=[lambda x: x in [test_constants.Constants.CAT, test_constants.Constants.DOG]])
@@ -43,7 +43,7 @@ animal_template = config_template._Template(
     nested_templates = [cat_template, dog_template]
 )
 
-base_config_template = config_template._Template(
+base_config_template = config_template.Template(
     fields=[
         config_field.Field(name=test_constants.Constants.NAME, types=[str], key=test_constants.Constants.NAME),
         config_field.Field(name=test_constants.Constants.SURNAME, types=[str], key=test_constants.Constants.SURNAME),
